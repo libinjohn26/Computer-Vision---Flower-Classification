85fa05052164b11df1f5475d3245628e9055478e
libinjohn26 <51414928+libinjohn26@users.noreply.github.com>
2020-02-09 20:31:26 +0100

    Final Update

--- cv_dataset.py
+++ cv_dataset.py
@@ -7,8 +7,10 @@ from scipy.io import loadmat
 from sklearn.model_selection import train_test_split
 import random
 import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-dataFile = "C:\\Users\\john\\Desktop\\Libin Docs\\Study Material\\Computer Vision\\Student Project\\jpg"
+dataFile = "C:\\Users\\john\\Desktop\\Libin Docs\\Study Material\\WiSe2019-20\\Computer Vision\\Student Project\\jpg"
 imageLabels = loadmat('imagelabels.mat')
 classValue = imageLabels['labels']
 labels = classValue[0]
@@ -17,12 +19,22 @@ path = os.path.join(dataFile)
 image_size = 100
 i = 0
 training_data = []
+data_temp = []
+
 for img in tqdm(os.listdir(path)):  # iterate over each image
     try:
         class_value = labels[i]
         img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_UNCHANGED)# ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        new_array = cv2.resize(img_array, (image_size, image_size))  # resize to normalize data size
+        RGB_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+        new_array = cv2.resize(RGB_img, (image_size, image_size))  # resize to normalize data size
         training_data.append([new_array, class_value])  # add this to our training_data
+
+        #flip_img = np.fliplr(new_array)
+        #training_data.append([flip_img, class_value])  # add this to our training_data
+
+        #rot_img = np.rot90(new_array)
+        #training_data.append([rot_img, class_value])  # add this to our training_data
+
         i = i + 1
         #plt.imshow(new_array, cmap='gray')  # graph it
         #plt.show()  # display!
@@ -35,24 +47,34 @@ for img in tqdm(os.listdir(path)):  # iterate over each image
 
 
 random.shuffle(training_data)
-
+print("Done Shuffle")
 X = []
 y = []
+x_temp = []
+y_temp = []
 
 for features,label in training_data:
     X.append(features)
     y.append(label)
 
 
+print("done seperation")
+
 X = np.array(X).reshape(-1, image_size, image_size, 3)
 y = np.array(y)
 
+print("Done Reshaping")
+
 x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+print("done splitting")
 
 print(len(x_train))
 print(len(x_test))
+
 print(len(y_train))
 print(len(y_test))
+
 
 pickle_out = open("X_train.pickle","wb")
 pickle.dump(x_train, pickle_out)
@@ -69,3 +91,5 @@ pickle_out.close()
 pickle_out = open("y_test.pickle","wb")
 pickle.dump(y_test, pickle_out)
 pickle_out.close()
+
+print("DONE")
--- cv_model_training.py
+++ cv_model_training.py
@@ -5,6 +5,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import TensorBoard
+from sklearn.model_selection import train_test_split
 
 import pickle
 
@@ -18,9 +20,24 @@ y_binary = to_categorical(y)
 
 X = X/255.0
 
+x_train,x_val,y_train,y_val = train_test_split(X,y_binary,test_size=0.2)
+
+
+datagen = ImageDataGenerator(
+    rotation_range=90,
+    #width_shift_range=0.2,
+    #height_shift_range=0.2,
+    horizontal_flip=True)
+    #vertical_flip=True)
+# compute quantities required for featurewise normalization
+# (std, mean, and principal components if ZCA whitening is applied)
+#datagen.fit(x_train)
+NAME = "cv_model_training"
+tensorboard = TensorBoard(log_dir="logs\{}".format(NAME))
+
 model = Sequential()
 
-model.add(Conv2D(32, (5, 5), input_shape=X.shape[1:]))
+model.add(Conv2D(32, (5, 5), input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -39,6 +56,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
 model.add(Dense(64))
+#model.add(Activation('relu'))
 
 model.add(Dense(103))
 model.add(Activation('softmax'))
@@ -47,6 +65,13 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y_binary, batch_size=32, epochs=10, validation_split=0.2)
+# fits the model on batches with real-time data augmentation:
+#model.fit_generator(datagen.flow(x_train, y_train, batch_size=32),
+#                    steps_per_epoch=len(x_train) / 32, epochs=20, validation_data=(x_val,y_val), 
+#                    callbacks=[tensorboard])
+
+model.fit(datagen.flow(x_train, y_train), epochs=20, validation_data=(x_val,y_val),
+        callbacks=[tensorboard])
+
 
 model.save("cv_cnn_model.model")
